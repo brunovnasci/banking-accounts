02ccdb9e0a9d8675a789b6fb55e829289ae9f5f4
Bruno Ventura <brunoventura80@gmail.com>
2025-03-22 16:47:46 -0300

    feat: create install sonar script to ec2 user data

---
+++ infra/scripts/install-sonar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo yum update
+sudo yum upgrade -y
+sudo yum -y install docker
+sudo service docker start
+
+sudo wget -P ./sonarqube/extensions/plugins https://github.com/mc1arke/sonarqube-community-branch-plugin/releases/download/1.23.0/sonarqube-community-branch-plugin-1.23.0.jar
+
+sudo docker run -p 80:9000 -v C:/Users/bruno/Projects/banking-accounts/sonarqube/extensions:/opt/sonarqube/extensions -e SONAR_WEB_JAVAADDITIONALOPTS="-javaagent:/opt/sonarqube/extensions/plugins/sonarqube-community-branch-plugin-1.23.0.jar=web" -e SONAR_CE_JAVAADDITIONALOPTS="-javaagent:/opt/sonarqube/extensions/plugins/sonarqube-community-branch-plugin-1.23.0.jar=ce" sonarqube:25.3.0.104237-community
